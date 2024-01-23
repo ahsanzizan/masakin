4ebea7f602ceb83930892b2d9c169ad89f97a83c
Ahsan <ahsanaz461@gmail.com>
2024-01-24 06:49:48 +0700

    feat: Detect first launch for onboarding

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,31 @@
 import { Redirect } from "expo-router";
 import { useSession } from "../lib/auth";
+import { useEffect, useState } from "react";
+import { getSecureItem, setSecureItem } from "../utils/secureStoreManager";
+import OnBoarding from "./onboarding";
 
-// TODO: implement onboarding
 export default function Root() {
   const { loggedIn } = useSession();
+  const [firstLaunch, setFirstLaunch] = useState<boolean | null>(null);
 
-  if (!loggedIn) return <Redirect href={"/login"} />;
+  useEffect(() => {
+    const init = async () => {
+      const getFirstLaunch = await getSecureItem("firstLaunch");
+
+      if (getFirstLaunch === null) {
+        setFirstLaunch(true);
+        setSecureItem("firstLaunch", "false");
+      } else {
+        setFirstLaunch(false);
+      }
+    };
+
+    init();
+  }, []);
+
+  if (!loggedIn && !firstLaunch) return <Redirect href={"/login"} />;
+
+  if (firstLaunch) return <OnBoarding />;
 
   return <Redirect href={"/home"} />;
 }
