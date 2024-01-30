f76116f0d36283c0dc52c1eccb35a9ced0d3e1ca
Ahsan <ahsanaz461@gmail.com>
2024-01-30 11:16:36 +0700

    feat: Add on-boarding page

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,3 +1,85 @@
+import {
+  Image,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Colors from "../constants/Colors";
+import { router } from "expo-router";
+
 export default function OnBoarding() {
-  return <></>;
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "space-between",
+        backgroundColor: Colors.dark.background,
+      }}
+    >
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: 104,
+        }}
+      >
+        <Image
+          source={require("../assets/images/icon.png")}
+          style={{ width: 80, height: 80, marginBottom: 14 }}
+        />
+        <Text
+          style={{ color: Colors.dark.text, fontWeight: "bold", fontSize: 18 }}
+        >
+          Your Modern Recipes Repository
+        </Text>
+      </View>
+      <View style={{ marginBottom: 84 }}>
+        <View style={{ marginBottom: 64 }}>
+          <Text
+            style={{
+              color: Colors.dark.text,
+              fontWeight: "bold",
+              fontSize: 42,
+              textAlign: "center",
+              marginBottom: 20,
+            }}
+          >
+            Let's Do Some Cooking!
+          </Text>
+          <Text
+            style={{
+              color: Colors.dark.text,
+              fontSize: 16,
+              textAlign: "center",
+            }}
+          >
+            Simplest way to find the tasty recipes
+          </Text>
+        </View>
+        <TouchableOpacity
+          style={{
+            paddingHorizontal: 32,
+            paddingVertical: 16,
+            backgroundColor: Colors.dark.primary,
+            borderRadius: 12,
+          }}
+          onPress={() => router.push("/home")}
+        >
+          <Text
+            style={{
+              color: Colors.dark.text,
+              fontSize: 16,
+              textAlign: "center",
+              fontWeight: "bold",
+            }}
+          >
+            Get Started
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
 }
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,22 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintColorLight = "#2f95dc";
+const tintColorDark = "#fff";
+const primary = "#E23E3E";
 
 export default {
   light: {
-    text: '#000',
-    background: '#fff',
+    text: "#000",
+    background: "#fff",
     tint: tintColorLight,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorLight,
+    primary,
   },
   dark: {
-    text: '#fff',
-    background: '#000',
+    text: "#fff",
+    background: "#000",
     tint: tintColorDark,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorDark,
+    primary,
   },
 };
