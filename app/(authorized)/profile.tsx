7a93afc77aa9b62221ff24a4ea272d4c2838de2b
Ahsan <ahsanaz461@gmail.com>
2024-02-04 18:00:07 +0700

    feat: Add profile page

---
+++ app/(authorized)/profile.tsx
@@ -0,0 +1,116 @@
+import { router } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import {
+  Image,
+  SafeAreaView,
+  ScrollView,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { LargeP, P, SmallP } from "../../components/Text";
+import FilterIcon from "../../components/icons/FilterIcon";
+import SearchIcon from "../../components/icons/SearchIcon";
+import Colors from "../../constants/Colors";
+import { useSession } from "../../lib/auth";
+import dimensions from "../../utils/dimensions";
+import MoreIcon from "../../components/icons/MoreIcon";
+
+export default function Home() {
+  const { user } = useSession();
+
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "space-between",
+        backgroundColor: Colors.light.background,
+      }}
+    >
+      <StatusBar />
+      <ScrollView
+        style={{
+          width: dimensions.windows.width,
+        }}
+        contentContainerStyle={{ paddingHorizontal: 30, paddingVertical: 94 }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          {/* Placeholder only */}
+          <View></View>
+          {/* Placeholder only */}
+          <LargeP style={{ fontWeight: "bold" }}>Profile</LargeP>
+          <TouchableOpacity>
+            <MoreIcon />
+          </TouchableOpacity>
+        </View>
+        <View style={{ paddingVertical: 10 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              marginBottom: 18,
+            }}
+          >
+            <Image
+              source={
+                user.avatar
+                  ? {
+                      uri: user.avatar,
+                    }
+                  : require("../../assets/images/profile_picture_placeholder.jpg")
+              }
+              style={{ width: 84, height: 84, borderRadius: 100 }}
+            />
+            <View
+              style={{ flexDirection: "row", gap: 18, alignItems: "center" }}
+            >
+              <View
+                style={{
+                  flexDirection: "column",
+                  gap: 2,
+                  alignItems: "center",
+                }}
+              >
+                <SmallP style={{ color: Colors.neutral }}>Recipes</SmallP>
+                <LargeP style={{ fontWeight: "bold" }}>4</LargeP>
+              </View>
+              <View
+                style={{
+                  flexDirection: "column",
+                  gap: 2,
+                  alignItems: "center",
+                }}
+              >
+                <SmallP style={{ color: Colors.neutral }}>Followers</SmallP>
+                <LargeP style={{ fontWeight: "bold" }}>25M</LargeP>
+              </View>
+              <View
+                style={{
+                  flexDirection: "column",
+                  gap: 2,
+                  alignItems: "center",
+                }}
+              >
+                <SmallP style={{ color: Colors.neutral }}>Following</SmallP>
+                <LargeP style={{ fontWeight: "bold" }}>1</LargeP>
+              </View>
+            </View>
+          </View>
+          <View>
+            <P style={{ fontWeight: "bold", marginBottom: 10 }}>Chef Abidin</P>
+            <SmallP>
+              Private Chef That Passionate about food and life 🥘🍲🍝🍱
+            </SmallP>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
