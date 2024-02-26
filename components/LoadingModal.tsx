e75e8198b1b663a74c933db316dad4929adf2dae
Ahsan <ahsanaz461@gmail.com>
2024-02-26 13:48:51 +0700

    feat: Add Loading component

---
+++ components/LoadingModal.tsx
@@ -0,0 +1,23 @@
+import Colors from "@constants/Colors";
+import { ActivityIndicator } from "react-native";
+
+export default function LoadingModal({ isActive }: { isActive?: boolean }) {
+  return (
+    <ActivityIndicator
+      animating={isActive}
+      hidesWhenStopped={false}
+      size={80}
+      color={Colors.primary}
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        justifyContent: "center",
+        alignItems: "center",
+        zIndex: isActive ? 1000 : -1,
+      }}
+    />
+  );
+}
